51175ba79e4251f0a1d7016a20c20976859c2952
전지우 <jjiwoo28@gmail.com>
2023-06-13 16:20:36 +0900

    jjiwoo추가 됌

--- src/test.java
+++ src/test.java
@@ -2,7 +2,7 @@ public class test{
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("github Test");
-		System.out.println("github Test");
+		System.out.println("jjiwoo");
 		
 	}
 }
